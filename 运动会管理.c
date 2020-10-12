a757af4b2e1909edba6064ece94097e49ddb9997
jibiao <jibiaozhang@qq.com>
2020-10-13 00:32:41 +0800

    增加运动员录入，成绩录入，优化部分输出效果

--- 运动会管理.c
+++ 运动会管理.c
@@ -6,8 +6,6 @@
 	Description: 
 */
 
-#define DEBUG  
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h> 
@@ -18,7 +16,6 @@ struct Athlete
 	char name[10];
 	char organization[10];
 	char gender[10];
-//	struct Project *project_pointer[10];
 	int score[10];
 };
 
@@ -26,8 +23,8 @@ struct Athlete
 //运动项目定义
 struct Project
 {
-	char name[10];
-	char site[10];
+	char name[20];
+	char site[20];
 	int month;	// 时间也分月日小时分秒 
 	int day;
 	int hour;
@@ -42,22 +39,25 @@ struct Project
 };
 
 
-
 // 功能模块 
 void project_publish(struct Project *p[])
 {
 	int sign=1;
 	int i=0;
-
+	int j=0; 
+	
+	//获取项目数量
 	while(p[i])
 	    i += 1;
 	    
 	while(sign)
 	{
-		struct Project project;
-		p[i] = (struct Project *)malloc(sizeof(struct Project));	
-
-		printf("1.运动会项目发布\
+		//申请一片指向结构体的内存空间，并负责给指针，结构体的指针数组初始化。 
+		p[i] = (struct Project *)malloc(sizeof(struct Project));		
+		for(;j<100;j++)
+			p[i]->athlete[j] = 0; 
+			
+		printf("\n<项目发布>\
     		\n您正在输入第%d个项目，请依次输入(以空格或回车分隔):\
 			\n* 项目名称\
 			\n* 项目场地\
@@ -70,7 +70,7 @@ void project_publish(struct Project *p[])
 			\n* 参数人数下限（人数不足项目将被取消)\
 			\n* 参数人数上限\n", i+1);
 			
-			scanf("%s%s%d%d%d%d%d%s%d%d",
+		scanf("%s%s%d%d%d%d%d%s%d%d",
 			p[i]->name,
 			p[i]->site,
 			&(p[i]->month),
@@ -82,44 +82,80 @@ void project_publish(struct Project *p[])
 			&(p[i]->down),
 			&(p[i]->up));
 									
-			// 增加检测输入正确性代码 
-			
-		struct Athlete athlete1;
-		strcpy(athlete1.name,"熬夜敲代码的我");
-		
-		struct Athlete athlete2;
-		strcpy(athlete2.name,"方一帆");
-		
-		struct Athlete athlete3;
-		strcpy(athlete3.name,"武勇涛");
-		
-		struct Athlete athlete4;
-		strcpy(athlete4.name,".");
-		
-		p[i]->athlete[0] = &athlete1; // 将指针指向运动员
-		p[i]->athlete[1] = &athlete2;
-		p[i]->athlete[2] = &athlete3;
-		p[i]->athlete[3] = &athlete4;
-		
-		printf("\n\n尝试查询运动员：\
-		\n1:%s\
-		\n2:%s\
-		\n3:%s\
-		\n4:%s\n\n",
-		p[i]->athlete[0]->name,
-		p[i]->athlete[1]->name,
-		p[i]->athlete[2]->name,
-		p[i]->athlete[3]->name);
+			// 增加检测输入正确性代码 	
 		
 		sign=0;	
 
 	}
 }
 
-void athlete_checkin()
+
+//运动员报名登记 
+void athlete_checkin(struct Project *p[])
 {
+	int sign=1;
+	int i=0;
+	int j=0;
+	int project_num=0; //已存在的项目数量 
+	int num; // 欲参加的项目数量 
+	int input; // 用户输入的序号 
+	 
+	//获取项目数量,并输出项目名称 
+	printf("\n已发布的项目：\n"); 
+	while(p[i])
+		{
+			printf("%d:%10s\t",i+1,p[i]->name); 
+	    	project_num += 1;
+	    	i += 1; 
+	    	if (i%5==0) printf("\n");
+		}
+	    
+	
+	while(sign)
+	{
+		struct Athlete *athlete_p;
+		athlete_p = (struct Athlete *)malloc(sizeof(struct Athlete));	
+
+		printf("\n输入运动员姓名、性别、单位，以空格或回车分隔：");
+									
+		// 增加检测输入正确性代码 ，能否报名成功 
+		
+		scanf("%s%s%s", athlete_p->name, athlete_p->gender, athlete_p->organization);
+		
+		printf("输入运动员<%s>要参加的下项目数量：",athlete_p->name);
+		scanf("%d",&num);
+		printf("输入项目序号");
+		for(i=0;i<num;i++)
+		{
+			scanf("%d",&input);
+			if (0<input && input<=project_num)
+				{
+					//获取当前项目已存在运动员的数量 
+					while((p[input-1]->athlete[j]))
+						j += 1;
+					//将运动员添加到当前项目 
+					p[input-1]->athlete[j]=athlete_p;
+					j =0;
+				}
+			else printf("\n输入序号对应的项目不存在！"); 
+		}
+		
+//		printf("\n\n尝试查询运动员：\
+//		\n1:%s\
+//		\n2:%s\
+//		\n3:%s\
+//		\n4:%s\n\n",
+//		p[input-1]->athlete[0]->name,
+//		p[input-1]->athlete[1]->name,
+//		p[input-1]->athlete[2]->name,
+//		p[input-1]->athlete[3]->name);
+		
+		sign=0;	
+
+	}
 	
 }
+
 
 //重构下面的orderbook_generation函数需要使用的交换两个指针的功能 
 void swap(struct Project **a, struct Project **b)
@@ -130,6 +166,7 @@ void swap(struct Project **a, struct Project **b)
 	*b = temp;
 }
 
+
 void orderbook_generation(struct Project *p[])
 {
 	//复制一份项目指针数组
@@ -137,17 +174,17 @@ void orderbook_generation(struct Project *p[])
 	memcpy(order,p,sizeof(struct Project *)*100);
 	
 	//获取项目数量
-	int project_qua=0;
-	while(p[project_qua])
-		project_qua += 1;
+	int project_num=0;
+	while(p[project_num])
+		project_num += 1;
 	
 	//对项目按时间顺序排序, 冒泡排序 
 	int i,j;
-	for(i=0;i<project_qua-1;i++)
+	for(i=0;i<project_num-1;i++)
 	{
 		order[i];
 		order[j];
-		for(j=i+1;j<project_qua;j++)
+		for(j=i+1;j<project_num;j++)
 			{
 				if (order[i]->month > order[j]->month)
 					swap(&order[i],&order[j]);
@@ -166,37 +203,65 @@ void orderbook_generation(struct Project *p[])
 			}
 	}
 	
-	for(i=0;i<project_qua;i++) // 待规划输出样式 ，尽量变横 
+	for(i=0;i<project_num;i++) // 待规划输出样式 ，尽量变横 
 		{
-			printf("\n第%d场比赛\
-			\n项目名称：%10s\
-			\n项目场地：%10s\
-			\n比赛月份：%10d\
-			\n日期：%14d\
-			\n小时：%14d\
-			\n分钟：%14d\
-			\n秒：%16d\
-			\n成绩单位：%10s\
-			\n参数人数下限：%6d\
-			\n参数人数上限：%6d\n",
+			printf("\n第<%d>场:%s %s %2d月%2d日%.2d:",
 			i+1,
 			order[i]->name,
 			order[i]->site,
 			order[i]->month,
 			order[i]->day,
-			order[i]->hour,
+			order[i]->hour);
+			
+			printf("%.2d:%.2d 以<%s>为成绩单位 最少%d人 最多%d人\n",
 			order[i]->minute, 
 			order[i]->second,
 			order[i]->unit,
 			order[i]->down,
 			order[i]->up);
-		}
-		
+		}		
 }
 
-void grade_input()
+void grade_input(struct Project *p[])
 {
+	int i=0;
+	int input;
+	int project_num=0;
 	
+	printf("\n已发布的项目：\n"); 
+	while(p[i])
+		{
+			printf("%d:%10s\t",i+1,p[i]->name); 
+	    	project_num += 1;
+	    	i += 1; 
+	    	if (i%5==0) printf("\n");
+		}
+		
+	printf("\n输入要录入的项目序号：");
+	scanf("%d",&input);
+	if (0<input && input<=project_num)
+		{
+			printf("输入第%d个项目<%s>的成绩：\
+			\n参赛的运动员有：\n",input,p[input-1]->name);
+			i = 0;
+			while(p[input-1]->athlete[i])
+				{
+					printf("%d:%8s\t",i+1,p[input-1]->athlete[i]->name);
+					i += 1;
+					if (i%5==0) printf("\n");
+				}
+			i=0;
+			printf("\n多个运动员以空格或回车分隔：\n");
+			while(project_num)
+				{
+					//运动员成绩数组和项目序号对应 
+					scanf("%d",&(p[input-1]->athlete[i]->score[input-1]));
+					project_num -= 1;
+				}
+			printf("输入成功。\n");
+				
+		}
+	else printf("输入的序号对应的项目不存在！\n");
 }
 
 void sportsmeet_card()
@@ -214,11 +279,6 @@ void data_backup()
 	
 }
 
-void quit()
-{
-	
-}
-
 // 主函数：作为主界面，实现获取用户输入并导航至相应功能。 
 int main()
 {
@@ -226,31 +286,32 @@ int main()
 	int input; //用户输入的序号 
 	int i=0;
 	
-	struct Project *list[100]={0};	
+	//定义指向项目的指针数组 
+	struct Project *list[100]={0}; 
 	
 	while(sign)
 		{
 			printf("<<学校运动会管理系统>>\n"); 
-			printf("1. 参数项目发布\n"
+			printf( "1. 项目发布\n"
 					"2. 运动员报名登记\n"
 					"3. 秩序册生成\n"
 					"4. 成绩录入\n"
 					"5. 运动会成绩册\n"
 					"6. 系统设置\n"
 					"7. 数据备份\n"
-					"8. 退出系统\n");
-			printf("\n输入序号以使用相应功能:"); 
+					"8. 退出系统");
+			printf("\n输入序号:"); 
 			scanf("%d",&input);
 			switch(input)
 				{
 					case 1:{project_publish(list);break;}
-					case 2:{athlete_checkin();break;}
+					case 2:{athlete_checkin(list);break;}
 					case 3:{orderbook_generation(list);break;}
-					case 4:{grade_input();break; }
+					case 4:{grade_input(list);break; }
 					case 5:{sportsmeet_card();break;} 
 					case 6:{seting();break;}
 					case 7:{data_backup();break;}
-					case 8:{quit();break;}
+					case 8:{sign=0;break;}
 					default:printf("未找到序号%d，输入有误，重新输入！\n\n",input); 
 				} 
 		}
